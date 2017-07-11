a08901eaae3b441702ad9ba101f9e072031b5aed
Steve Stone <stoneflynm@gmail.com>
2017-07-11 16:13:44 -0600

    added more text to first paragraph

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 	</head>
 	<body style="background-color: mediumorchid">
 		<h1>Dannielle Bojorquez</h1>
-		<p> Hello, my name is Dannielle Bojorquez. I was currently working as vet tech in Moab Utah when I saw the Deep
-			Dive Bootcamp.  I am looking forward to learning a lot in the class</p>
+		<p> Hello, my name is Dannielle Bojorquez. I was currently working as vet tech in Moab, Utah when I saw the Deep
+			Dive Bootcamp.  Moab was a very cool place to live but I got tired of seeing mountain bikes all the time.
+			I am looking forward to learning a lot in the class.</p>
 
 		<p> I am a mom of 2 dogs who love to lay around.  They are the light of my life and I don't know what I would do
 			without them. In my spare time I love to do wedding cakes for some extra money!</p>
