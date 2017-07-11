33f3be7537895a9c918771a8fff83a5576213ce2
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-07-10 19:42:38 -0600

    completed first assignment

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dannielle Bojorquez</title>
+	</head>
+	<body>
+		<p> Hello, my name is Dannielle Bojorquez. I was currently working in the emergency room as a registrar
+			when this deep dive coders caught my eye.I am looking forward to learning and taking on this new opportunity.  </p>
+
+		<p> I am a mom of 2 who likes to do be creative when possible. I will do many DIY projects to keep
+			myself busy and entertained. I love the accomplishment when a project is done </p>
+		<img src="image/dannielle.jpg" width ="200"/>
+	</body>
+</html>
